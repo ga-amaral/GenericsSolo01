141510bc8cec8a98feecb3d746792aa7149f6475
Gabriel Amaral <amaralgabriel123@gmail.com>
2020-07-29 20:07:03 -0300

    Primeira Versao do Generics

---
+++ GenericsSolo01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsSolo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                printService.AddValue(value);
+            }
+
+            printService.Print();
+
+            Console.WriteLine("First: " + printService.First());
+
+
+        }
+    }
+}
